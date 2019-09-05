6fb336fc90ce6df7ccb23f228e1a1d949ef55984
shibin <shibin888888888@gmail.com>
2019-09-05 17:11:47 +0800

    change quantity in cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,7 @@ class ProductsController < ApplicationController
 current_cart.add_product_to_cart(@product)
 
 
-   flash[:notice] = "已经将{@product.title}加入购物车"
+   flash[:notice] = "已经将#{@product.title}加入购物车"
  else
      flash[:warning] = "你的购物车内已有此物品"
    end
